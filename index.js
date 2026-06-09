79b1453defc47f82f57a52c5b75e5cb85a31a453
rizan <bhandarizan129@gmail.com>
2026-06-09 23:32:05 +0545

    feat: Refactored signin and signup pages along with model improvement

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+const UserRoute = require('./routes/user');
 
 const app = express();
 const PORT = 8000;
 
+mongoose.connect('mongodb://localhost:27017/blog_chunks')
+.then((e) => console.log('Connected to MongoDB')).catch(err => console.log(err));
+
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views'));
 
-app.get('/', (req, res) => {
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => { 
   res.render('home');
 });
+
+app.use('/user', UserRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
---
+++ models/user.js
@@ -0,0 +1,74 @@
+const { createHmac, randomBytes} = require("crypto");
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    salt:{
+        type: String,
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+    
+    profileImageURL: {
+        type: String,
+        default: '/images/user_avatar.png',
+    },
+
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user',
+    },
+},
+{
+    timestamps: true,}
+);
+
+userSchema.pre('save', function() {
+    const user = this;
+    if (!user.isModified('password')) return;
+    
+    const salt = randomBytes(16).toString('hex');
+    const hashedPassword = createHmac('sha256', salt)
+    .update(user.password)
+    .digest('hex');
+
+    this.salt = salt;   
+    this.password = hashedPassword;
+   
+});
+
+userSchema.static('matchPassword', async function (email, password) { 
+    const user = await this.findOne({ email });
+    if (!user) throw new Error('User not found');
+
+    const salt =  user.salt;
+    const hashedPassword = user.password;
+
+    const userProvidedHash = createHmac('sha256', salt)
+    .update(password)
+    .digest('hex');
+    if (userProvidedHash !== hashedPassword) throw new Error('Incorrect password');
+    
+    return user;
+
+
+} );
+
+
+const User = model('User', userSchema);
+
+module.exports = User;
